792fd5d649a02c9d364956f8ae520dd5e09bd4d0
Lee <jessy1092@gmail.com>
2013-10-06 17:14:55 +0800

    set api_keys to test

--- src/test/java/org/lee/hackpad/jackpad/JackpadClientTest.java
+++ src/test/java/org/lee/hackpad/jackpad/JackpadClientTest.java
@@ -1,14 +1,44 @@
 package org.lee.hackpad.jackpad;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.junit.Before;
 
 public class JackpadClientTest
 {
+	private static final String API_KEYS_FILE = "api_keys.txt";
+	
+	private String HACKPAD_CLIENT_ID;
+	private String HACKPAD_SECRET;
 	private JackpadClient jackpadClient;
 	
 	@Before
 	public void setUp()
 	{
-		
+		setApiKey();
+		jackpadClient = new JackpadClient(HACKPAD_CLIENT_ID, HACKPAD_SECRET);
+		jackpadClient.build();
+	}
+	
+	public void setApiKey()
+	{
+		try
+		{
+			FileReader fr = new FileReader(API_KEYS_FILE);
+			BufferedReader br = new BufferedReader(fr);
+			String line;
+			line = br.readLine();
+			String[] para= line.split(" ");
+			HACKPAD_CLIENT_ID = para[0];
+			HACKPAD_SECRET = para[1];
+			br.close();
+			fr.close();
+		}
+		catch(IOException e)
+		{
+			System.out.println(e.toString());
+		}
 	}
 }
